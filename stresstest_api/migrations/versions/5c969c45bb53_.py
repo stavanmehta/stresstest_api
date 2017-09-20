8fab8858d975938fc9a786c16d0fcb05424541d2
Greg Ivanov <greg.ivanov@openx.com>
2017-09-19 21:42:19 -0700

    Adding migrations

---
+++ stresstest_api/migrations/versions/5c969c45bb53_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5c969c45bb53
+Revises: 
+Create Date: 2017-09-18 21:14:42.373347
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c969c45bb53'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ stresstest_api/migrations/versions/d9d5eb4d6211_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d9d5eb4d6211
+Revises: 
+Create Date: 2017-09-18 23:11:49.969558
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd9d5eb4d6211'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
